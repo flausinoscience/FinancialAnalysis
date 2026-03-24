c063f60eca52b273ec011df49319b4b19986ea7e
flausinoscience <flausino.science@gmail.com>
2026-03-23 21:03:48 -0300

    add create dimensions step in the dw pipeline

--- ETL/pipeline/warehouse.py
+++ ETL/pipeline/warehouse.py
@@ -1,11 +1,25 @@
-from ..warehouse.dims import dim_customer
+from pathlib import Path
+from sqlalchemy import text
+from src.db.ingestion import run_sql_file
+from src.db.connection import get_engine
 
-def create_dim_customer():
-    print('---------------Customer dimension creation')
-    c_data = dim_customer.extract()
-    c_t_data = dim_customer.transform(c_data)
-    print('---------------Create customer [OK]')
+
+def create_dw_schema(engine):
+    with engine.begin() as conn:
+        conn.execute(
+            text('CREATE SCHEMA IF NOT EXISTS dw;')
+        )
     
 
+def create_dimension_tables(engine):
+    ddl_script_dimensions = Path('ETL/warehouse/dims/create_dims.sql').absolute()
+    run_sql_file(engine, ddl_script_dimensions)
+
+
 if __name__ == '__main__':
-    create_dim_customer()
+    engine = get_engine()
+
+    create_dw_schema(engine)
+
+    create_dimension_tables(engine)
+
---
+++ ETL/warehouse/dims/create_dims.sql
@@ -0,0 +1,71 @@
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+-- SCD Type 2
+CREATE TABLE IF NOT EXISTS dw.dim_account(
+    account_sk SERIAL PRIMARY KEY,
+    account_id UUID NOT NULL,
+    account_type VARCHAR(45),
+    account_status VARCHAR(45),
+    account_country VARCHAR(45),
+    currency CHAR(3),
+    opened_at TIMESTAMP,
+    owner_email VARCHAR(255) NOT NULL,
+    owner_first_name VARCHAR(255),
+    owner_last_name VARCHAR(255),
+    owner_country VARCHAR(255),
+    owner_birth_date DATE,
+    owner_sign_up_at TIMESTAMP,
+
+    valid_from TIMESTAMP NOT NULL,
+    valid_to TIMESTAMP,
+    is_current BOOLEAN NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS dw.dim_brokerage(
+    brokerage_sk SERIAL PRIMARY KEY,
+    brokerage_id UUID NOT NULL,
+    name VARCHAR(255),
+    country VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS dw.dim_asset(
+    asset_sk SERIAL PRIMARY KEY,
+    asset_id UUID NOT NULL,
+    ticker TEXT NOT NULL,
+    name TEXT NOT NULL,
+    type TEXT NOT NULL,
+    currency CHAR(3)
+);
+
+-- Junk Dimension
+CREATE TABLE IF NOT EXISTS dw.dim_trade_info(
+    trade_info_sk SERIAL PRIMARY KEY,
+    trade_type_id SMALLINT NOT NULL,
+    trade_type_name VARCHAR(255) NOT NULL,
+    is_flagged BOOLEAN,
+    reason_flag TEXT
+);
+
+CREATE TABLE IF NOT EXISTS dw.dim_date(
+    date_sk INT PRIMARY KEY, -- YYYYMMDD
+    full_date DATE NOT NULL,
+
+    day INT NOT NULL,
+    day_name VARCHAR(10) NOT NULL,
+    day_of_week INT NOT NULL,
+    day_of_year INT NOT NULL,
+
+    week_of_year INT NOT NULL,
+
+    month INT NOT NULL,
+    month_name VARCHAR(10) NOT NULL,
+    month_start_date DATE NOT NULL,
+    month_end_date DATE NOT NULL,
+
+    quarter INT NOT NULL,
+    quarter_name CHAR(2) NOT NULL,
+
+    year INT NOT NULL,
+
+    is_weekend BOOLEAN NOT NULL
+);
